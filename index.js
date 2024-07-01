3eb9d2b3a12f1d9ed5ebc3fed0c78ecd88b5252d
CalebKiK <karimicaleb@gmail.com>
2024-07-02 01:01:27 +0300

    Complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const divide = function() {
+    let a = 2000;
+    let b =100;
+
+    console.log(a/b);
+
+}
+
+const square = (x) => {
+    let x = 2;
+
+    console.log(x**2);
+}
+
+const add = (a, b) => {
+    let a = 3;
+    let b = 4;
+
+    console.log (a+b);
+}
